6050c9d347e3db18492ed6eeadbe33e3e723ec49
eugen neagoe <eneagoe@gmail.com>
2019-03-28 23:26:36 +0200

    Add Fibonacci example functions

---
+++ fib_automemo.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+# auto-memoization, no memory limits
+@lru_cache(maxsize=None)
+def fib(n: int) -> int:
+    if n < 2:
+        return n
+
+    return fib(n - 2) + fib(n - 1)
+
+
+if __name__ == "__main__":
+    print(fib(500))
